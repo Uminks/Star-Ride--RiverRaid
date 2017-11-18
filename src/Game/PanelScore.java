537d248be2a07ff42fd313afa317bc06e7812a65
Brayan Montañez <bbmo.181299@gmail.com>
2017-11-18 00:13:27 -0400

    "Medificado panel de registros y algunas optimizaciones de código"

--- src/Game/PanelScore.java
+++ src/Game/PanelScore.java
@@ -5,8 +5,13 @@
  */
 package Game;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -18,40 +23,93 @@ import javax.swing.Timer;
  */
 public class PanelScore extends JPanel{
     
+    private Font StarJedi;
+    private JLabel iconFuel;
     private JProgressBar fuel;
     private int intTiempo, intScore, intLives;
+    private JLabel iconTiempo, iconScore, iconLives;
     private JLabel tiempo, score, lives;
     private Timer timerFuel, timerTiempo;
     
     public PanelScore(){
         super.setLayout(null);
-        super.setBounds(600, 0, 200, 600);       
+        super.setBackground(Color.black);
+        super.setBounds(600, 0, 200, 600); 
+        
     }
     
     /**
      * Inicia los componentes del panel del Score
      */
     public void initComponents(){
-        fuel = new JProgressBar(0);
-        fuel.setBounds(25, 50, 150, 30);  
-        fuel.setValue(100);
+        int extra = 30;
+        //FUENTE
+        StarJedi = this.cargarFuente("../Starjedi.ttf");
+        
+        /**
+         * TODO TIEMPO
+         */
+        iconTiempo = new JLabel("tiempo");               
+        iconTiempo.setBounds(40, 10+extra, 200, 50);
+        iconTiempo.setForeground(Color.yellow);
+        iconTiempo.setFont(StarJedi);
         
         intTiempo = 0;
         tiempo = new JLabel(String.valueOf(intTiempo));
-        tiempo.setBounds(95, 5, 100, 30);
+        tiempo.setBounds(82, 80+extra, 100, 30);
+        tiempo.setForeground(Color.yellow);
+        tiempo.setFont(StarJedi.deriveFont(1, 22f));
+        
+        /**
+         * TODO SCORE
+         */
+        iconScore = new JLabel("score");               
+        iconScore.setBounds(45, 130+extra, 200, 50);
+        iconScore.setForeground(Color.yellow);
+        iconScore.setFont(StarJedi);
         
         intScore = 0;
         score = new JLabel(String.valueOf(intScore));
-        score.setBounds(98, 110, 100, 10);
+        score.setForeground(Color.yellow);
+        score.setFont(StarJedi.deriveFont(1, 22f));
+        score.setBounds(90, 200+extra, 100, 30);
+        
+        /**
+         * TODO COMBUSTIBLE
+         */
+        iconFuel = new JLabel();               
+        iconFuel.setBounds(10, 250+extra, 200, 50);
+        iconFuel.setForeground(Color.yellow);
+        iconFuel.setFont(StarJedi.deriveFont(1, 22f));
+        iconFuel.setText("Combustible");
+        
+        fuel = new JProgressBar(0);
+        fuel.setBounds(25, 325+extra, 150, 30);  
+        fuel.setValue(100);
+        
+        /**
+         * TODO VIDAS
+         */
+        iconLives = new JLabel();               
+        iconLives.setBounds(43, 380+extra, 200, 50);
+        iconLives.setForeground(Color.yellow);
+        iconLives.setFont(StarJedi);
+        iconLives.setText("vidas");
         
         intLives = 3;
         lives = new JLabel(String.valueOf(intLives));
-        lives.setBounds(99, 130, 10, 10);
+        lives.setForeground(Color.yellow);
+        lives.setFont(StarJedi.deriveFont(1, 22f));
+        lives.setBounds(90, 450+extra, 100, 30);
         
-        super.add(fuel);
-        super.add(tiempo);
-        super.add(score);
-        super.add(lives);
+        super.add(iconTiempo, 0);
+        super.add(tiempo, 0);
+        super.add(iconScore, 0);
+        super.add(score, 0);
+        super.add(iconFuel, 0);
+        super.add(fuel, 0);
+        super.add(iconLives, 0);
+        super.add(lives, 0);
     }
     
     /**
@@ -150,5 +208,30 @@ public class PanelScore extends JPanel{
      */
     public Timer getTimerTiempo() {
         return timerTiempo;
+    }
+    
+     /**
+     * Metodo para Cargar fuentes personalizadas
+     * @param ruta Ruta del Archivo
+     * @return retorna una fuente personalizada
+     */
+    public Font cargarFuente(String ruta){
+        Font fuente=null; 
+        InputStream entrada = getClass().getResourceAsStream(ruta);
+        
+        try {                 
+            fuente = Font.createFont(Font.TRUETYPE_FONT, entrada);
+            fuente = fuente.deriveFont(1,28f);
+
+        } catch (FontFormatException ex) {
+            fuente = new Font("Arial", Font.PLAIN, 19);  
+            System.out.println("Error de Formato");
+        } catch (IOException ex) {
+            fuente = new Font("Arial", Font.PLAIN, 19);
+            System.out.println(entrada);
+            System.out.println("Error de Entrada y Salida de Datos");
+        }
+     
+        return fuente;  
     }
 }
